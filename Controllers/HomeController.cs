8a63923ef1bab01d0ebbb4c8eeaf0061a68e8bf3
Nick D'Oria <17dorin@gmail.com>
2021-04-06 16:55:37 -0400

    Users can create project groups

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace MockDevOps.Controllers
@@ -12,6 +13,7 @@ namespace MockDevOps.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DevOpsContext _context = new DevOpsContext();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -20,7 +22,21 @@ namespace MockDevOps.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            //Gets list of all project ids associated with currently logged in user
+            List<ProjectUser> UsersProjectIds = new List<ProjectUser>();
+            List<Project> UsersProjects = new List<Project>();
+            if(_context.ProjectUsers.Count() != 0)
+            {
+                UsersProjectIds = _context.ProjectUsers.Where(x => x.UserId == User.FindFirst(ClaimTypes.NameIdentifier).Value).ToList();
+            }
+            else
+            {
+                return View(UsersProjects);
+            }
+            //Gets list of actual project objects based on the above list
+            UsersProjects = _context.Projects.Where(x => UsersProjectIds.Select(x => x.ProjectId).Contains(x.Id)).ToList();
+
+            return View(UsersProjects);
         }
 
         public IActionResult Privacy()
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MockDevOps.Data;
+using MockDevOps.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +32,10 @@ namespace MockDevOps
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDbContext<DevOpsContext>(options =>
+                options.UseSqlServer(
+                        Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
